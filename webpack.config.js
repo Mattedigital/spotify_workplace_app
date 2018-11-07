c3b05a1f9fe5552132108939e9d3b101443eb66f
James Robinson <jrobind@outlook.com>
2018-11-07 21:23:58 +0000

    begin basic scaffold implementation

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './app/index.jsx'],
+    output: {
+        path: '/',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/},
+            {test: /\.scss$/, use: ['style-loader', {loader: 'css-loader', options: {
+                modules: true,
+                localIdentName: '[hash:base64:5]__[local]'
+            }}, 'sass-loader']},
+            {test: /\.(png|jpg|svg)$/, loader: 'url-loader?linit=8192'}
+        ]
+    },
+    devServer: {
+        historyApiFallback: true  
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'app/index.html',
+            inject: false
+        })
+    ],
+    mode: 'development'
+} 
